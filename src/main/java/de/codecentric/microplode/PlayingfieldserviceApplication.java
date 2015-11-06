028b212fe769418176c6dcc091aacd28cf1ce64f
ThomasJaspers <tritom42@googlemail.com>
2015-11-06 16:20:01 +0100

    Project initialized with Spring Initializer.

---
+++ src/main/java/de/codecentric/microplode/PlayingfieldserviceApplication.java
@@ -0,0 +1,12 @@
+package de.codecentric.microplode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayingfieldserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayingfieldserviceApplication.class, args);
+    }
+}
