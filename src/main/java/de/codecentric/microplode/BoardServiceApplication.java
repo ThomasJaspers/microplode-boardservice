e1d0f6b6f0e6547cdb71e23ed1d0b1154e954669
Jonas Hecht <jonas.hecht@codecentric.de>
2015-12-11 13:14:09 +0100

    You don´t need to Autowire the Configuration, Spring Boot detects it automatically :)

--- src/main/java/de/codecentric/microplode/BoardServiceApplication.java
+++ src/main/java/de/codecentric/microplode/BoardServiceApplication.java
@@ -1,7 +1,5 @@
 package de.codecentric.microplode;
 
-import de.codecentric.microplode.configuration.MessagingConfiguration;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,9 +8,6 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan("de.codecentric.microplode")
 public class BoardServiceApplication {
-
-    @Autowired
-    MessagingConfiguration messagingConfiguration;
 
     public static void main(String[] args) {
         SpringApplication.run(BoardServiceApplication.class, args);
